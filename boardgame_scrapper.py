c6b3efd3e0c4b9985b1c6cda3fd49e138c71a529
Cory Simpkins <simpki3@clemson.edu>
2019-03-26 15:28:14 -0400

    realized normal way wasnt getting prices, switched to selenium

--- boardgame_scrapper.py
+++ boardgame_scrapper.py
@@ -1,23 +1,39 @@
-import urllib.request
+# import urllib.request
 import os
-import time
-import datetime
+# import time
+# import datetime
 
 if not os.path.exists("html_files"):
 	os.mkdir("html_files")
 
-# for i in range(1060):
-# 	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-# 	print(str(i) + ": " + current_time_stamp)
-# 	f = open("html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
-# 	response = urllib.request.urlopen('https://coinmarketcap.com/')
-# 	html = response.read()
-# 	f.write(html)
-# 	f.close()
-# 	time.sleep(10)
+# # for i in range(1060):
+# # 	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+# # 	print(str(i) + ": " + current_time_stamp)
+# # 	f = open("html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+# # 	response = urllib.request.urlopen('https://coinmarketcap.com/')
+# # 	html = response.read()
+# # 	f.write(html)
+# # 	f.close()
+# # 	time.sleep(10)
 
-f = open("html_files/boardgamegeek1.html", "wb")
-response = urllib.request.urlopen('https://boardgamegeek.com/browse/boardgame')
-html = response.read()
+# f = open("html_files/boardgamegeek1.html", "wb")
+# response = urllib.request.urlopen('https://boardgamegeek.com/browse/boardgame')
+# html = response.read()
+# f.write(html)
+# f.close()
+
+from selenium import webdriver
+import time
+ 
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--test-type")
+driver = webdriver.Chrome(options=options)
+driver.get('https://boardgamegeek.com/browse/boardgame/page/1')
+time.sleep(3)
+html = driver.page_source 
+f = open("html_files/boardgamegeek1.html", "w")
 f.write(html)
 f.close()
