01757e4efc95ec6ab82bf5dd00674c58f83fe19c
Cory Simpkins <simpki3@clemson.edu>
2019-03-26 15:43:10 -0400

    try loop for 5 times

--- boardgame_scrapper.py
+++ boardgame_scrapper.py
@@ -31,9 +31,11 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 driver = webdriver.Chrome(options=options)
-driver.get('https://boardgamegeek.com/browse/boardgame/page/1')
-time.sleep(3)
-html = driver.page_source 
-f = open("html_files/boardgamegeek1.html", "w")
-f.write(html)
-f.close()
+
+for i in range(5):
+	driver.get('https://boardgamegeek.com/browse/boardgame/page/' + str(i + 1))
+	time.sleep(3) #lets the prices load
+	html = driver.page_source 
+	f = open("html_files/boardgamegeek" + str(1+ i) + ".html", "w", encoding="utf8")
+	f.write(html)
+	f.close()
