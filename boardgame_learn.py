a426c4043a8883987ffa0c2367f4e9ded240d69b
Cory Simpkins <simpki3@clemson.edu>
2019-04-11 12:19:04 -0400

    linear model to predict avg price for games without prices

---
+++ boardgame_learn.py
@@ -0,0 +1,38 @@
+from sklearn import linear_model
+import pandas as pd
+import numpy as np 
+
+dataset = pd.read_csv("parsed_files/boardgamegeek_dataset.csv")
+
+#print (dataset.head(20))
+
+trainer = dataset[dataset.avg_price.notnull()]
+trainer = trainer.dropna()
+
+target = trainer.avg_price.values
+
+#print(target)
+
+data = trainer[['avg_rating', 'geek_rating', 'game_rank', 'game_year', 'num_votes']].copy()
+
+#print(data)
+
+regression = linear_model.LinearRegression()
+
+regression.fit(data, target)
+
+
+test = dataset.loc[dataset['avg_price'].isnull()]
+
+X = test[['avg_rating', 'geek_rating', 'game_rank', 'game_year', 'num_votes']].copy()
+
+X = X.dropna()
+
+#print(X)
+#print(len(X))
+#print(len(data))
+#print(trainer.head(20))
+
+results = regression.predict(X)
+
+print(results)
